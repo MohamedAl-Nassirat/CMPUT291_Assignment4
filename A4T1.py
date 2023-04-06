99d5a8da5b3688c125e99ff737f669aba5fe38f3
abdullahinur2828 <abdul.nur2828@gmail.com>
2023-04-05 23:19:05 -0600

    we got Task 1

---
+++ A4T1.py
@@ -0,0 +1,26 @@
+import pymongo
+import bson.json_util 
+import sys
+
+port_number = int(sys.argv[1])
+
+
+with open('songwriters.json', encoding='utf-8') as f:
+    songwriters_data = bson.json_util.loads(f.read())
+
+# Load the recordings data from the JSON file
+with open('recordings.json', encoding='utf-8') as f:
+    recordings_data = bson.json_util.loads(f.read())
+
+# Set up a connection to the MongoDB server
+client = pymongo.MongoClient(f"mongodb://localhost:{port_number}/")
+
+
+
+db = client["A4dbNorm"]
+db.create_collection("songwriters")
+db.create_collection("recordings")
+
+db["songwriters"].insert_many(songwriters_data)
+db["recordings"].insert_many(recordings_data)
+
